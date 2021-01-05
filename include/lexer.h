53099cd2d411ed0912d3fe17e1dc45b7b736aedf
Josh Blatt <thejoshblatt@yahoo.com>
2021-01-04 21:07:44 -0500

    Refactoring lexer data structures to header

--- include/lexer.h
+++ include/lexer.h
@@ -26,4 +26,59 @@ const char *case_keyword = "case";
 const char *default_keyword = "default";
 const char **keywords;
 
+typedef enum TokenKind {
+    TOKEN_EOF = 0,
+    // Reserve first 128 values for one-char tokens
+    TOKEN_LAST_CHAR = 127,
+    TOKEN_KEYWORD,
+    TOKEN_INT,
+    TOKEN_FLOAT,
+    TOKEN_STR,
+    TOKEN_NAME,
+    TOKEN_LSHIFT,
+    TOKEN_RSHIFT,
+    TOKEN_EQ,
+    TOKEN_NOTEQ,
+    TOKEN_LTEQ,
+    TOKEN_GTEQ,
+    TOKEN_AND,
+    TOKEN_OR,
+    TOKEN_INC,
+    TOKEN_DEC,
+    TOKEN_COLON_ASSIGN,
+    TOKEN_ADD_ASSIGN,
+    TOKEN_FIRST_ASSIGN = TOKEN_ADD_ASSIGN,
+    TOKEN_SUB_ASSIGN,
+    TOKEN_OR_ASSIGN,
+    TOKEN_AND_ASSIGN,
+    TOKEN_XOR_ASSIGN,
+    TOKEN_LSHIFT_ASSIGN,
+    TOKEN_RSHIFT_ASSIGN,
+    TOKEN_MUL_ASSIGN,
+    TOKEN_DIV_ASSIGN,
+    TOKEN_MOD_ASSIGN,
+    TOKEN_LAST_ASSIGN = TOKEN_MOD_ASSIGN,
+} TokenKind;
+
+typedef enum TokenMode {
+    TOKENMODE_NONE,
+    TOKENMODE_HEX,
+    TOKENMODE_BIN,
+    TOKENMODE_OCT,
+    TOKENMODE_CHAR,
+} TokenMode;
+
+typedef struct Token {
+    TokenKind kind;
+    TokenMode mode;
+    const char *start;
+    const char *end;
+    union {
+        uint64_t int_val;
+        double float_val;
+        const char *str_val;
+        const char *name;
+    };
+} Token;
+
 #endif //MOME_LEXER_H
--- src/lexer.c
+++ src/lexer.c
@@ -32,48 +32,6 @@ void initKeywords() {
     buf_push(keywords, default_keyword);
 }
 
-typedef enum TokenKind {
-    TOKEN_EOF = 0,
-    // Reserve first 128 values for one-char tokens
-    TOKEN_LAST_CHAR = 127,
-    TOKEN_KEYWORD,
-    TOKEN_INT,
-    TOKEN_FLOAT,
-    TOKEN_STR,
-    TOKEN_NAME,
-    TOKEN_LSHIFT,
-    TOKEN_RSHIFT,
-    TOKEN_EQ,
-    TOKEN_NOTEQ,
-    TOKEN_LTEQ,
-    TOKEN_GTEQ,
-    TOKEN_AND,
-    TOKEN_OR,
-    TOKEN_INC,
-    TOKEN_DEC,
-    TOKEN_COLON_ASSIGN,
-    TOKEN_ADD_ASSIGN,
-    TOKEN_FIRST_ASSIGN = TOKEN_ADD_ASSIGN,
-    TOKEN_SUB_ASSIGN,
-    TOKEN_OR_ASSIGN,
-    TOKEN_AND_ASSIGN,
-    TOKEN_XOR_ASSIGN,
-    TOKEN_LSHIFT_ASSIGN,
-    TOKEN_RSHIFT_ASSIGN,
-    TOKEN_MUL_ASSIGN,
-    TOKEN_DIV_ASSIGN,
-    TOKEN_MOD_ASSIGN,
-    TOKEN_LAST_ASSIGN = TOKEN_MOD_ASSIGN,
-} TokenKind;
-
-typedef enum TokenMode {
-    TOKENMODE_NONE,
-    TOKENMODE_HEX,
-    TOKENMODE_BIN,
-    TOKENMODE_OCT,
-    TOKENMODE_CHAR,
-} TokenMode;
-
 const char *token_kind_names[] = {
     [TOKEN_EOF] = "EOF",
     [TOKEN_INT] = "int",
@@ -101,16 +59,3 @@ const char *token_kind_names[] = {
     [TOKEN_DIV_ASSIGN] = "/=",
     [TOKEN_MOD_ASSIGN] = "%=",
 };
-
-typedef struct Token {
-    TokenKind kind;
-    TokenMode mode;
-    const char *start;
-    const char *end;
-    union {
-        uint64_t int_val;
-        double float_val;
-        const char *str_val;
-        const char *name;
-    };
-} Token;
